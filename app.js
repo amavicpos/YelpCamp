c3d3fcf4a9999148e41c56599d24cd99e82cd8e7
amavicpos <100805638+amavicpos@users.noreply.github.com>
2023-07-14 14:31:21 +0100

    Add MongoAtlas url

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const helmet = require('helmet');
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
+const dbUrl = process.env.DB_URL;
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
